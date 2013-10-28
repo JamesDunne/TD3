6adfd03b6b29796e427e11fbe015c4824fe89e91
James Dunne <james.jdunne@gmail.com>
2013-10-27 20:30:49 -0500

    Basic OpenGL framework in place via OpenTK.

---
+++ TD3/TD3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace TD3
+{
+    public class TD3 : GameWindow
+    {
+        public TD3() : base(800, 600)
+        {
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            float aspect_ratio = Width / (float)Height;
+            Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perpective);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (Keyboard[OpenTK.Input.Key.Escape])
+                this.Exit();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
+
+        /// <summary>
+        /// Entry point of this example.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (TD3 main = new TD3())
+            {
+                main.Title = "Test Drive 3 visualizer";
+                main.Run(30.0, 0.0);
+            }
+        }
+    }
+}
